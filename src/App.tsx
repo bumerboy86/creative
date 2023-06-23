865ebbc88ff49cd3196ea1758e7be7a594820cda
ruslan <bumerboy86@gmail.com>
2023-06-23 09:37:58 +0600

    добавлен значек

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout.tsx";
 import Home from "./containers/Home/Home.tsx";
+import About from "./containers/About/About.tsx";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
             <Routes>
                 <Route element={<Layout />}>
                     <Route index element={<Home />}/>
-                    <Route path={"/about"} element={<div className="bg-white">About</div>}/>
+                    <Route path={"/about"} element={<About />}/>
                     <Route path={"/contact"} element={<div className="bg-white">Contacts</div>}/>
                 </Route>
             </Routes>
