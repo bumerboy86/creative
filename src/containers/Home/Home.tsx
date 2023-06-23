1f1a9fbd9bc3231400474d496aba02a2b12104ad
ruslan <bumerboy86@gmail.com>
2023-06-23 16:22:59 +0600

    добавлена ссылка на переход в гитхаб

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
     return (
         <div className="w-full h-full flex flex-col justify-center pt-[5rem] pl-[2rem] sm:pl-[4rem] pb-[6rem]">
@@ -5,7 +7,14 @@ const Home = () => {
                 Creative
             </h1>
             <h3 className="text-white text-[2rem] font-bold">Design Studio</h3>
-            <button className="w-[10rem] py-[6px] bg-[#98ec65] rounded-3xl hover:bg-[#81e047] mt-[1rem]">Explore Now</button>
+            <a 
+                className="w-[10rem] py-[6px] bg-[#98ec65] text-center rounded-3xl hover:bg-[#81e047] mt-[1rem]"
+                href="https://github.com/bumerboy86"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+            Explore Now
+            </a>
         </div>
     )
 }
