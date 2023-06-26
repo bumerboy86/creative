04e52f66e3415724aa4b235e17b9d98deb5a749d
ruslan <bumerboy86@gmail.com>
2023-06-26 17:36:47 +0600

    изменена надпись в футере

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white text-center py-4 fixed bottom-0 w-full">
-            <p>&copy; 2023 Your Website. All rights reserved.</p>
+            <p>&copy; 2023 Website. All rights reserved.</p>
         </footer>
     );
 };
